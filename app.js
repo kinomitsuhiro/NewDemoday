375da41edf265227dd10b8ca63c84d0795d2d7ef
木野允寛 <kino@kinomakotokan-no-MacBook-Pro.local>
2016-09-26 19:20:44 +0900

    1st

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-setInterval(function() {
-  console.log("Hello IoT from isaax");
-}, 5000);
+var mraa = require("mraa");
+var led = new mraa.Gpio(13); 
+var v = 0;
+setInterval(function(){
+	v = v ? 0:1;
+	if(v){
+		console.log("LED ON with isaax");
+		led.dir(mraa.DIR_OUT);
+	}else{
+		console.log("LED OFF with isaax");
+	}led.write(v);},4000);
